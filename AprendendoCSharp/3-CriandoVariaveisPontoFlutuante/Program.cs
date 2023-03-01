0124fbddaef464d3abafc06bf10037b32246f2a8
Felipe B. de Moura <fittato@gmail.com>
2023-03-01 13:21:30 -0300

    Finalizada a aula 03 do curso C# explorando a linguagem

---
+++ AprendendoCSharp/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario;
+
+        salario = 3000.10;
+
+        salario = 3000; // Variável do tipo double aceitam número inteiro
+
+        // int idade;
+        // idade = 27.8; // Variável int não aceita double
+
+        double idade;
+
+        // idade = 7 / 5; // Apesar de ser float, o C# devolve a resposta 1, já que o numerador e o denominador são inteiros
+
+        idade = 7.0 / 5;
+
+        double peso = 4.0;
+
+        int quantidade = 10;
+
+        Console.WriteLine(peso * quantidade);
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+
+    }
+}
---
+++ AprendendoCSharp/4-ConversoesEOutrosTipos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 4 - Conversões e Outros Tipos");
+
+        double salario;
+
+        salario = 3000.15;
+
+        Console.WriteLine(salario);
+
+        int salarioInteiro;
+
+        salarioInteiro = (int)salario; // Cast de double para inteiro
+
+        Console.WriteLine(salarioInteiro);
+
+        // int x;
+        // x = 2000000000000; // Int guarda até 32 bits, logo número grandes não compilarão
+
+        long x = 2000000000000000000;
+
+        Console.WriteLine(x);
+
+        // short y;
+
+        // y = 15000;
+
+        short y = 15000;
+
+        Console.WriteLine(y);
+
+        float altura;
+
+        altura = 1.62f; // O f serve para avisar o compilador de que a variável deve ser float, e não double, já que o compilador avisa do problema da falta de precisão do float em relação ao double
+
+        Console.WriteLine(altura);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
