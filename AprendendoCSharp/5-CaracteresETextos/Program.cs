e5d3298921e637c51d6dba72a32446210e69f59c
Felipe B. de Moura <fittato@gmail.com>
2023-03-02 12:56:15 -0300

    Finalizada a aula 04 do curso C# explorando a linguagem

---
+++ AprendendoCSharp/5-CaracteresETextos/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a'; // em caracteres são usadas somente aspas simples. Já em strings são utilizadas as aspas duplas
+        Console.WriteLine(letra);
+
+        letra = (char)65;
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        letra = (char)(86 + 1);
+        Console.WriteLine(letra);
+
+        string primeiraFrase = "Alura - Cursos de tecnologia ";
+        Console.WriteLine(primeiraFrase + 2022);
+
+        string vazia = "";
+        Console.WriteLine(vazia);
+
+        // letra = ''; o C# não guarda char ou string vazia
+
+        letra = ' ';
+
+        string cursos = @"Cursos disponíveis:
+- Go
+- C#
+- Python
+- Java";
+        Console.WriteLine(cursos);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
