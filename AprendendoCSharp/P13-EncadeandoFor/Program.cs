5fc904f755209ac9d5023c0f9a11a807af294f72
Felipe B. de Moura <fittato@gmail.com>
2023-03-06 13:24:31 -0300

    Finalizada a aula 06 do curso C# explorando a linguagem

---
+++ AprendendoCSharp/P13-EncadeandoFor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 13 - Encadeando For");
+
+        for (int contadorLinhas = 0; contadorLinhas <=9; contadorLinhas++)
+        {
+            for (int contadorColuna = 0; contadorColuna <= contadorLinhas; contadorColuna++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
