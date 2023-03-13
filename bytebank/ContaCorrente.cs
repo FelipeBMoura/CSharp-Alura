e7785078e73a93d088ee3da5e06a291bcb58b030
Felipe B. de Moura <fittato@gmail.com>
2023-03-13 12:36:48 -0300

    Finalizada a aula 02 do curso C# orientação a objetos

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -10,5 +10,38 @@ namespace bytebank
         public string titular;
         public double saldo;
 
+		public void depositar(double valor)
+		{
+			this.saldo += valor;
+		}
+
+		public bool sacar(double valor)
+		{
+			if (valor <= this.saldo)
+			{
+				this.saldo -= valor;
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+
+		}
+
+		public bool transferir(double valor, ContaCorrente destino)
+		{
+			if(this.saldo < valor)
+			{
+				return false;
+			}
+			else
+			{
+				this.sacar(valor);
+				destino.depositar(valor);
+				return true;
+			}
+		}
+
 	}
 }
--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -8,4 +8,35 @@ contaDoAndre.conta = "1010-X";
 contaDoAndre.saldo = 100;
 
 Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+
+// contaDoAndre.depositar(100);
+
+// Console.WriteLine("Saldo da conta do André pós depósito = " + contaDoAndre.saldo);
+
+// if (contaDoAndre.sacar(300) == true)
+// {
+
+// Console.WriteLine("Saldo da conta do André pós saque = " + contaDoAndre.saldo);
+
+//}
+//else
+//{
+// Console.WriteLine("Saldo insuficiente para saque.");
+//}
+
+ContaCorrente contaDaMaria = new ContaCorrente();
+contaDaMaria.titular = "Maria Souza";
+contaDaMaria.numero_agencia = 17;
+contaDaMaria.conta = "1010-5";
+contaDaMaria.saldo = 350;
+
+
+Console.WriteLine("Salda da conta da Maria = " + contaDaMaria.saldo);
+
+contaDoAndre.transferir(50, contaDaMaria);
+
+Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+
+Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
 Console.ReadLine();
