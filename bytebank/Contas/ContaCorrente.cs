2e821bddf1a4c4c3e72b3892b154d9ce4f5da333
Felipe B. de Moura <fittato@gmail.com>
2023-03-16 13:17:40 -0300

    Finalizada a aula 05 do curso C# orientação a objetos

--- bytebank/Contas/ContaCorrente.cs
+++ bytebank/Contas/ContaCorrente.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-using bytebank.Titular
+using bytebank.Titular;
 
 namespace bytebank.Contas
 {
 
 	public class ContaCorrente
 	{
-		public int numero_agencia;
-        public string conta;      
+        private int numero_agencia;
+		public int Numero_agencia
+		{
+			get { return this.numero_agencia; }
+			set {
+				if (value > 0)
+					this.numero_agencia = value;
+			}
+		}
+        // private string? conta;
+		public string? Conta { get; set; } 
+
         public double saldo=100;
 
-		public Cliente titular;
+        public Cliente? Titular { get; set; };
 
         public void depositar(double valor)
 		{
@@ -45,15 +55,22 @@ namespace bytebank.Contas
 			}
 		}
 
-        public void ExibirDadosDaConta()
-        {
-            Console.WriteLine("Titular :" + titular);
-            Console.WriteLine("Conta :" + conta);
-            Console.WriteLine("Número Agência :" + numero_agencia);
-            Console.WriteLine("Nome Agência :" + nome_agencia);
-            Console.WriteLine("Saldo: " + saldo);
-        }
+		public void SetSaldo(double valor)
+		{
+			if(valor < 0)
+			{
+				return;
+			}
+			else
+			{
+				this.saldo = valor;
+			}
+		}
 
-
+		public double GetSaldo()
+		{
+			return this.saldo;
+		}
+		
     }
 }
