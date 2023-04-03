d8158e79d992115c349d8f34047b5d63acf29c80
Felipe B. de Moura <fittato@gmail.com>
2023-04-03 12:51:49 -0300

    Finalizada a aula 04 do curso C# entendendo exceções

--- bytebank/Program.cs
+++ bytebank/Program.cs
@@ -5,17 +5,19 @@ using bytebank.Titular;
 
 try
 {
-    ContaCorrente conta1 = new ContaCorrente(278, "1234-X");
+    ContaCorrente conta1 = new ContaCorrente(0, "1234-X");
+    /*
     conta1.sacar(50);
     Console.WriteLine(conta1.GetSaldo());
     conta1.sacar(150);
     Console.WriteLine(conta1.GetSaldo());
-
+    */
 }
 catch (ArgumentException ex)
 {
     Console.WriteLine("Parâmetro com erro: " + ex.ParamName); // mostra qual argumento está lançando a exceção
     Console.WriteLine("Não é possível criar uma conta com o número de agência menor ou igual a zero!");
+    Console.WriteLine(ex.StackTrace);
     Console.WriteLine(ex.Message); // Mostra a mensagem do throw
 }
 catch(SaldoInsuficienteException ex)
