24e251f6bc54027e53ba6529bcf8f6f2ce9f9777
Felipe B. de Moura <fittato@gmail.com>
2023-04-25 12:36:53 -0300

    Finalizada a aula 07 do curso Design Patterns C#

---
+++ CursoDesignPatterns/NotaFiscal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoDesignPatterns
+{
+    public class NotaFiscal
+    {
+        public String RazaoSocial { get; set; }
+        public String Cnpj { get; set; }
+        public DateTime DataDeEmissao { get; set; }
+        public double ValorBruto { get; set; }
+        public double Impostos { get; set; }
+        public IList<ItemDaNota> Itens { get; set; }
+        public String Observacoes { get; set; }
+
+        public NotaFiscal(
+            String razaosocial,
+            String cnpj,
+            DateTime dataDeEmissao,
+            double valorBruto,
+            double impostos,
+            IList<ItemDaNota> itens,
+            String observacoes
+            ) 
+        {
+            this.RazaoSocial = razaosocial;
+            this.Cnpj = cnpj;
+            this.DataDeEmissao = dataDeEmissao;
+            this.ValorBruto = valorBruto;
+            this.Impostos = impostos;
+            this.Itens = itens;
+            this.Observacoes = observacoes;
+        }
+
+    }
+}
--- CursoDesignPatterns/Program.cs
+++ CursoDesignPatterns/Program.cs
@@ -4,19 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Orcamento reforma = new Orcamento(500);
+            NotaFiscalBuilder criador = new NotaFiscalBuilder();
+            criador
+            .ParaEmpresa("Caelum Ensino e Inovacao")
+            .ComCnpj("23.456.789/0001-12")
+            .ComItem(new ItemDaNota("item 1", 100.0))
+            .ComItem(new ItemDaNota("item 2", 200.0))
+            .NaDataAtual()
+            .ComObservacoes("uma obs qualquer");
 
-            Console.WriteLine(reforma.Valor);
+            criador.AdicionaAcao(new EnviadorDeEmail());
+            criador.AdicionaAcao(new NotaFiscalDao());
+            criador.AdicionaAcao(new EnviadorDeSms());
 
-            reforma.AplicaDescontoExtra();
-            Console.WriteLine(reforma.Valor);
+            NotaFiscal nf = criador.Constroi();
 
-            reforma.Aprova();
-
-            reforma.AplicaDescontoExtra();
-            Console.WriteLine(reforma.Valor);
-
-            reforma.Finaliza();
+            Console.WriteLine(nf.ValorBruto);
+            Console.WriteLine(nf.Impostos) ;
 
             Console.ReadKey();
         }
