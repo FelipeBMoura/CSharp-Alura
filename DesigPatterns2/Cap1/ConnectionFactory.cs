1336c561187051bce424ec1ccd5dc784602cd8b6
Felipe B. de Moura <fittato@gmail.com>
2023-04-27 12:31:38 -0300

    Finalizada a aula 01 do curso Design Patterns C# II

---
+++ DesigPatterns2/Cap1/ConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesigPatterns2.Cap1
+{
+    internal class ConnectionFactory
+    {
+        public IDbConnection GetConnection()
+        {
+            IDbConnection connexao = new SqlConnection();
+            connexao.ConnectionString = "User Id=root;Password=;Server=localhost;Database=banco";
+            connexao.Open();
+
+            return connexao;
+        }
+    }
+}
---
+++ DesigPatterns2/Program.cs
@@ -0,0 +1,17 @@
+﻿using DesigPatterns2.Cap1;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DesignPatterns2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+            
+            IDbCommand comando = connexao.CreateCommand();
+            comando.CommandText = "select * from tabela";
+        }
+    }
+}
